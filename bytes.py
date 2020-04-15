4930895aae90c377f9ea5b9892a26a2de09a2d10
newcarvalho <new.figueiredo@gmail.com>
2020-04-15 17:36:32 -0300

    removendo arquivos duplicados e adicionando a aula bytes

---
+++ bytes.py
@@ -0,0 +1,9 @@
+#-------------Bytes-----------------
+#escrever em byte é simples :coloca um "b" antes da string
+frase=b"oi sumida"
+print(frase)
+
+#como strings tambem podemos manipular os bytes
+#podemos usar comandos como decode ou encode... mas no curso ele nao da muita enfase
+#ta mais para um conteudo informacional.
+
