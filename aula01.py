9ef98152c3dbe551613128bc9a74eba02b92b246
newcarvalho <new.figueiredo@gmail.com>
2020-04-15 14:51:48 -0300

    subindo o resumo da primeira aula de python

---
+++ aula01.py
@@ -0,0 +1,31 @@
+#------OVERVIEW---------
+#inteiro
+10
+#chamando um numero binario (0b)
+0b10
+#chamando um numero hexadecimal (0x)
+0x10
+#covertendo variavel em inteiro (int()):
+int(3.5)
+#covertendo variavel em decimal (float()):
+float(3)
+#escrevendo em notação cientifica (e):
+3e8
+1.61e-5
+#operador none:
+a = None
+a == None
+a
+ #operadores logicos: True e False
+ bool(0)  #apenas zero é considerado falso
+ bool(10)
+ bool(-10)
+ bool(0.0)
+ bool(0.225)
+ bool("") #apenas strings vazias é considerado falso
+ bool([]) #apenas listas vazias é considerado falso
+ bool([1,2,3])
+ bool("False") 
+ 
+ #--------RELATIONAL OPERATOR --------
+ 
