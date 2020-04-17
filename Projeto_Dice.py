223e82ee34f3c73138063adbac1abff9dda435c6
newcarvalho <new.figueiredo@gmail.com>
2020-04-17 01:16:03 -0300

    meu primeiro programa python dice gerator - melhor forma de jogar seu RPG de mesa ;)

---
+++ Projeto_Dice.py
@@ -0,0 +1,51 @@
+from random import choice 
+
+Dados=[]
+Lados=[]
+gamelocal=True
+gameglobal=True
+
+print("Fala galera partiu jogar esse RPG... só escolherem seus Dados e partiiiu!!")
+
+while gameglobal==True:
+
+    ndados=int(input("De quantos dados voce precisa?: "))
+    nlados=int(input("Os dado que voce precisa é de quantos lados ?: "))
+    
+    for lados in range(nlados):
+        Lados.append(lados+1)
+
+
+    while gamelocal==True:
+            nndados=ndados
+
+            if ndados >=2 and nlados >0 :
+                print("Lançando os Dados!!!!")
+                for dados in range(nndados):
+                    Dados.append(Lados)
+                    print(f"Valor do Dado Nº:{dados+1} é: {choice(Dados[dados])}")
+            elif ndados == 1 and nlados >0 :
+                print("Lançando o Dados!!!!")
+                print(f"Valor do Dado é: {choice(Lados)}")
+            elif ndados <=0 or nlados <=0 :
+                print("Não vale trollar o programinha :3 ")
+            
+            resposta=str(input("Vamos continuar nesse modo ?:((s) para sim ou (n) para nao):"))
+            if resposta == "s":
+                gamelocal=True
+            elif resposta == "n":
+                gamelocal=False
+        
+    resposta=str(input("Vamos continuar com o gerador de dado ?:((s) para sim ou (n) para nao):"))
+    if resposta == "s":
+        gameglobal=True
+        gamelocal=True
+    elif resposta == "n" :
+        gameglobal=False
+        print("Até a proxima!!!!")    
+    
+        
+'''
+print(Dados)
+print(choice(Lados))
+'''
