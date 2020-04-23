fd85de85bba348e55b0f165d8e741678b86c317b
newcarvalho <new.figueiredo@gmail.com>
2020-04-23 18:50:44 -0300

    AJUSTES NO PROJETO CHUTE_UM_NUMERO

--- Meus_Scripts/chute_o_numero.py
+++ Meus_Scripts/chute_o_numero.py
@@ -1,6 +1,10 @@
 from random import randint
 
-def Progresso(numeros_chutados):
+ME_ADVINHE = randint(1,100)
+CONTADOR = 0
+NUMEROS_CHUTADOS=set()
+
+def progress(numeros_chutados):
     progresso = []
     for i in range(1,101):
         if i in numeros_chutados:
@@ -17,26 +21,18 @@ print("SIMPLES, NÉ NÃO !?\n")
 switch_button=str(input("VAMOS COMEÇAR: (S)im OU (N)ão \n"))
 print("\n"+100*"-"+"\n")
 
-
-#------variaveis globais -------
-me_advinhe = randint(1,100)
-contador = 0
-numeros_chutados=set()
-#print(me_advinhe)
-#------------------------------
-
 while switch_button == "s" or switch_button == "S":
    
     print(100*"-"+"\n")
     chute=int(input("CHUTE UM NUMERO:\n "))
     
-    if chute not in numeros_chutados:
+    if chute not in NUMEROS_CHUTADOS:
         
-        contador += 1
-        numeros_chutados.add(chute)
+        CONTADOR += 1
+        NUMEROS_CHUTADOS.add(chute)
 
-        if chute == me_advinhe:
-            if contador <= 2:
+        if chute == ME_ADVINHE:
+            if CONTADOR <= 2:
                 print(100*"-"+"\n")
                 print("PUTA MERDAAA, VAI TOMAR NO SEEU CÚ!! ,BIXO CAGADO DO CARALHOOOOO!!!!\n ")
                 switch_button=False
@@ -44,25 +40,14 @@ while switch_button == "s" or switch_button == "S":
                 print(100*"-"+"\n")
                 print("AEWWWW CARRAAAIIII VOCE ACERTOUUUUU KKKKKK\n ")
                 switch_button=False
-        elif chute > me_advinhe - 10 and chute < me_advinhe + 10 :
-            if me_advinhe - chute >= -10 and me_advinhe - chute < 0:
-                print("EITAAAA VIADOOO QUE TA BEM PERTINHO DIMINUA MAIS UM POUQUINHO KKKKKK\n ") 
-            elif me_advinhe - chute > 0 and me_advinhe - chute <= 10:
-                print("EITAAAA VIADOOO QUE TA BEM PERTINHO AUMENTE MAIS UM POUQUINHO KKKKKK\n ") 
-                
-        elif chute > me_advinhe - 25 and chute < me_advinhe + 25 :
-            if me_advinhe - chute >= -25 and me_advinhe - chute < 0:
-                print("CARALHOOOO TA QUASE LÁ, DIMINUA O VALOR KKKKKK \n") 
-            elif me_advinhe - chute > 0 and me_advinhe - chute <= 25:
-                print("CARALHOOOO TA QUASE LÁ, AUMENTE O VALOR KKKKKK \n") 
-              
-        elif chute > me_advinhe + 40 and chute < me_advinhe - 40:
-            
-            print("VISH PASSOU FOI LONGE KKKKKK \n")
-        
-        
-        print(f"\nNUMERO DE TENTATIVAS: {contador}\n")
-        print(f"SEU PROGRESSO:\n {Progresso(numeros_chutados)} ")
+        else:
+            if ME_ADVINHE - chute < 0:
+                print("EITAAAA VIADOOO DIMINUA MAIS UM POUQUINHO KKKKKK\n ") 
+            elif ME_ADVINHE - chute > 0:
+                print("CARAI MANO AUMENTE MAIS AI KKKKKK\n ") 
+                    
+        print(f"\nNUMERO DE TENTATIVAS: {CONTADOR}\n")
+        print(f"SEU PROGRESSO:\n {progress(NUMEROS_CHUTADOS)} ")
         print(100*"-"+"\n")
 
     else:
