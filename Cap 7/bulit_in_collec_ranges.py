30de772ef2a31c809facd314b9249fc131aa4795
newcarvalho <new.figueiredo@gmail.com>
2020-04-23 15:28:02 -0300

    aula range

---
+++ Cap 7/bulit_in_collec_ranges.py
@@ -0,0 +1,12 @@
+#------------ range ---------
+'''
+metodo range representa uma sequencia aritimetica de numeros inteiros
+exemplo:
+'''
+print(range(5))
+for i in range(5)
+    print(i)
+
+print(range(5,10))
+print(list(range(5,10)))
+print(list(range(0,10,2))
