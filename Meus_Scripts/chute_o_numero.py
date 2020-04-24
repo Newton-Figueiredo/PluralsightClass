a29a90725660a13a0fda65523d4d424594357ae2
newcarvalho <new.figueiredo@gmail.com>
2020-04-23 22:18:22 -0300

    ajuste no projeto chute_o_numero

--- Meus_Scripts/chute_o_numero.py
+++ Meus_Scripts/chute_o_numero.py
@@ -1,7 +1,6 @@
 from random import randint
 
-def barra():
-    return print(100*"-"+"\n")
+
 def progress(numeros_chutados):
     progresso = []
     for i in range(1,101):
@@ -10,49 +9,52 @@ def progress(numeros_chutados):
         else:
             progresso.append(i)
     return progresso
-def main():
-    ME_ADVINHE = randint(1,100)
-    NUMEROS_CHUTADOS=set()
 
-    barra()
+
+def main():
+    me_advinhe = randint(1,100)
+    numeros_chutados=set()
+    barra = 100*"-"+"\n"
+
+    print(barra)
     print("SEJAM BEM VINDOS AO CHUTE UM NUMERO!!!\n ")
     print("O OBJETIVO DO JOGO:\nACERTAR UM NUMERO ENTRE 1 - 100 COM O MENOR NUMERO DE TENTATIVAS.\n ")
     print("SIMPLES, NÉ NÃO !?\n")
     switch_button=str(input("VAMOS COMEÇAR: (S)im OU (N)ão \n"))
-    barra()
+    print(barra)
 
     while switch_button in ["s","S"]:
     
-        barra()
+        print(barra)
         chute=int(input("CHUTE UM NUMERO:\n "))
 
-        if chute not in NUMEROS_CHUTADOS:
+        if chute not in numeros_chutados:
 
 
-            NUMEROS_CHUTADOS.add(chute)
+            numeros_chutados.add(chute)
 
-            if chute == ME_ADVINHE:
-                if len(NUMEROS_CHUTADOS) <= 2:
-                    barra()
+            if chute == me_advinhe:
+                if len(numeros_chutados) <= 2:
+                    print(barra)
                     print("PUTA MERDAAA, VAI TOMAR NO SEEU CÚ!! ,BIXO CAGADO DO CARALHOOOOO!!!!\n ")
                 else:
-                    barra()
+                    print(barra)
                     print("AEWWWW CARRAAAIIII VOCE ACERTOUUUUU KKKKKK\n ")
 
                 switch_button=False
 
             else:
-                if ME_ADVINHE - chute < 0:
+                if me_advinhe - chute < 0:
                     print("EITAAAA VIADOOO DIMINUA MAIS UM POUQUINHO KKKKKK\n ") 
-                elif ME_ADVINHE - chute > 0:
+                elif me_advinhe - chute > 0:
                     print("CARAI MANO AUMENTE MAIS AI KKKKKK\n ") 
 
-            print(f"\nNUMERO DE TENTATIVAS: {len(NUMEROS_CHUTADOS)}\n")
-            print(f"SEU PROGRESSO:\n {progress(NUMEROS_CHUTADOS)} ")
-            barra()
+            print(f"\nNUMERO DE TENTATIVAS: {len(numeros_chutados)}\n")
+            print(f"SEU PROGRESSO:\n {progress(numeros_chutados)} ")
+            print(barra)
 
         else:
-            barra()
+            print(barra)
             print("VOCE JÁ CHUTOU ESSE NUMERO KKKKKK\n ")
 
 main()
