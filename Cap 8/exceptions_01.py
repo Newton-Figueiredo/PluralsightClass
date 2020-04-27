bcda0fb158ffd1403652c6e86c977d2c198bd223
newcarvalho <new.figueiredo@gmail.com>
2020-04-27 16:00:26 -0300

    aula de exception

---
+++ Cap 8/exceptions_01.py
@@ -0,0 +1,6 @@
+#----------exception handling--- tratamento de exceçoes-----
+'''
+mecanismo para interromper o fluxo normal do programa e continuar no contexto do "erro/problema"
+circundante.
+
+'''
